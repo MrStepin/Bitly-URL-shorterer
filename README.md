977e92d9388f2f0e0906f9bfd155e3f502eaecd0
MrStepin <stanislavalex@mail.ru>
2018-12-27 23:55:28 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+Bitly URL shorterer
+
+Description how to work this script:
+This script is transfer your URL to Bitly URL.
+If you enter Bitly URL, script is output quantity of click to this URL during all time.
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+pip install -r requirements.txt
+
+How to use this script: 
+Execute in CMD this script with URLS as arguments
+For example: main.py https://www.google.com/
---
+++ main.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+load_dotenv()
+import argparse
+import requests
+import os
+from urllib.parse import urlparse
+import sys
+
+
+secret_token = os.getenv("TOKEN")
+token = {"Authorization": secret_token }
+url = "https://api-ssl.bitly.com/v4/bitlinks"
+users_link = {"long_url": "", "title": "new"}
+url_for_sum = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary"
+period_for_calc_clicks = {"unit":"day", "units":""}
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('urls', nargs='+')
+    return parser
+    
+def create_short_link(url, token, users_link):
+    short_link = requests.post(url, json = users_link, headers = token) 
+    
+    if short_link.status_code == 400:
+        return None
+    else:  
+        response = short_link.json()
+        link_bitly = response["link"]
+        return link_bitly
+
+def calculating_of_clicks_to_link(url_for_sum, token, users_link): 
+    summ_of_clicks = requests.get(url_for_sum.format(users_link["long_url"][7:]), params = period_for_calc_clicks, headers = token) 
+    
+    if summ_of_clicks.status_code == 404:
+        return None
+    else:   
+        response = summ_of_clicks.json()
+        total_clicks = response["total_clicks"]
+        return total_clicks
+
+if __name__ == '__main__': 
+    entered_links = createParser()
+    urls_space = entered_links.parse_args(sys.argv[1:])
+    for entered_urls in urls_space.urls:
+        users_link.update({"long_url": entered_urls})
+        if urlparse(users_link["long_url"]).netloc == "bit.ly":
+            total_clicks = calculating_of_clicks_to_link(url_for_sum, token, users_link) 
+        
+            if total_clicks == None:
+                print("Введена некорректная ссылка.")
+            else:
+                print("Всего кликов: {}".format(total_clicks))
+        else:
+            link_bitly = create_short_link(url, token, users_link)
+        
+            if link_bitly == None:
+                print("Введена некорректная ссылка.")
+            else:
+                print("Короткая ссылка: {}".format(link_bitly))
+    
+    
+
+ 
